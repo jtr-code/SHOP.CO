ce8fc52659c473c9aec8d1e042224224f545b6ff
jtr-code <jtrtricks@gmail.com>
2025-02-08 23:01:25 +0530

    add : global styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,21 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontSize: {
+        'head-large': 'clamp(2.25rem, 1.9rem + 1.75vw, 4rem)',
+        'head-medium': 'clamp(2rem, 1.8rem + 1vw, 3rem)',
+        'head-small': 'clamp(1.5rem, 1.35rem + 0.75vw, 2.25rem)',
+      },
+      fontFamily: {
+        'open-sans': ['var(--font-open-sans)'],
+      },
+      colors: {
+        'light-gray': '#F0F0F0',
+        'warm-gray': '#F0EEED'
+      },
+    },
+  },
   plugins: [],
 } satisfies Config;
