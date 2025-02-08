9476ce83348f4255e1a79b8000422f2ebb08828d
jtr-code <jtrtricks@gmail.com>
2025-02-09 00:37:53 +0530

    setup : husky,prettier,eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -10,16 +10,16 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        'head-large': 'clamp(2.25rem, 1.9rem + 1.75vw, 4rem)',
-        'head-medium': 'clamp(2rem, 1.8rem + 1vw, 3rem)',
-        'head-small': 'clamp(1.5rem, 1.35rem + 0.75vw, 2.25rem)',
+        "head-large": "clamp(2.25rem, 1.9rem + 1.75vw, 4rem)",
+        "head-medium": "clamp(2rem, 1.8rem + 1vw, 3rem)",
+        "head-small": "clamp(1.5rem, 1.35rem + 0.75vw, 2.25rem)",
       },
       fontFamily: {
-        'open-sans': ['var(--font-open-sans)'],
+        "open-sans": ["var(--font-open-sans)"],
       },
       colors: {
-        'light-gray': '#F0F0F0',
-        'warm-gray': '#F0EEED'
+        "light-gray": "#F0F0F0",
+        "warm-gray": "#F0EEED",
       },
     },
   },
