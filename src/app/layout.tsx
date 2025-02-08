e83e356d2981117f18c72e662d1df6d7d5740960
jtr-code <jtrtricks@gmail.com>
2025-02-09 01:35:23 +0530

    ui : footer and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,32 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
+import React from 'react';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 const openSans = Open_Sans({
-  variable: "--font-open-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800"],
+  variable: '--font-open-sans',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800']
 });
 
 export const metadata: Metadata = {
-  title: "Shop.Co",
-  description: "An ecommerce app",
+  title: 'Shop.Co',
+  description: 'An ecommerce app'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang='en' data-theme='light'>
       <body className={`${openSans.variable} font-open-sans antialiased`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
